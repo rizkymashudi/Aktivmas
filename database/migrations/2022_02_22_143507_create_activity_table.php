6e27f45d1745e8929374eeec423b900775eae9d6
rimas12 <rizky_mashudy@yahoo.co.id>
2022-02-22 21:54:53 +0700

    add db migration

---
+++ database/migrations/2022_02_22_143507_create_activity_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity', function (Blueprint $table) {
+            $table->id();
+            $table->string('poster');
+            $table->string('activity_name');
+            $table->string('performer');
+            $table->string('audience_type');
+            $table->string('activity_detail');
+            $table->date('activity_date');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity');
+    }
+}
